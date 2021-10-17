8c8a86ea2c8793a29f682286c522414b5e4975cd
python290821 <90146835+python290821@users.noreply.github.com>
2021-10-17 18:29:12 +0300

    Update program.py

--- program.py
+++ program.py
@@ -1,1 +1,11 @@
 # solution for homework
+# page 35-1
+_min = int(input('enter number: '))
+for i in range(1, 100):
+    _next = int(input('enter number: '))
+    if _next < _min:
+        print('not sorted!')
+        break
+    _min = _next
+if _next >= _min:
+    print('sorted')
