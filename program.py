d24225c5358e87c025aa2816da2a8ced51439459
python290821 <90146835+python290821@users.noreply.github.com>
2021-10-17 19:39:11 +0300

    Update program.py

--- program.py
+++ program.py
@@ -1,6 +1,11 @@
 
 import random
+
+my_score = 0
+computer_score = 0
 for i in range(3):
+
+    # i draw a card
     card1 = random.randint(2, 14)
     if card1 <= 10:
         print(card1)
@@ -12,4 +17,13 @@ for i in range(3):
         print('K')
     else: # card1 == 14:
         print('A')
+
+    #todo:
+    # computer draws card
+
+    # print who won or draw
+    # update score
+
+    # after 3 rounds print score for player and computer
+
 
